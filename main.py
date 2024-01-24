a954095ec38162a8974a8f52edc82a372ce1aa01
Aaron Beschorner <aaronbeschorner@gmail.com>
2024-01-24 09:06:40 -0500

    Dictionary with Crew Actions

--- main.py
+++ main.py
@@ -3,12 +3,29 @@ import random as rd
 import numpy as np
 
 # Create a 3D array (log) using NumPy
-def GenerateBoard(attendance):
+def GenerateLog(attendance):
     a = np.zeros((attendance, 18, 2))
     return a
+
+def PrintLog():
+    crewActions = {
+        1: "Product",
+        2: "Customer",
+        3: "Bagger",
+        4: "Helmsing",
+        5: "Lot Helmsing",
+        6: "Demo",
+        7: "Wine Demo",
+        8: "Art",
+        9: "Section Leading",
+        10: "Captain Meeting",
+        11: "Mentor Meeting",
+        12: "Bathroom Check"
+    }
 
 
 if __name__ == '__main__':
     crewNumber = int(input("Enter number of crew members:"))
-    board = GenerateBoard(crewNumber)
-    print(board)
+    log = GenerateLog(crewNumber)
+    log[0][0][0] = 1
+    print(log)
